80ce447438643b61d406bdde33d6555fc80111a3
Ravi kumar <1002kumarravi@gmail.com>
2022-04-14 15:08:08 +0530

    change videos data

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -3,19 +3,88 @@
  * You can add videos of your wish with different attributes
  * */
 
+ import { v4 as uuid } from "uuid";
+
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+    _id:"yQEondeGvKo",
+    title: "STRANGER THINGS Season 4",
+    category: "Trailer",
+    description: "STRANGER THINGS Season 4 Trailer (NEW, 2022)",
+    creator: "Netflix",
+    uploadDate: "12-04-2022",
+    url: "https://youtu.be/yQEondeGvKo"
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    _id:"-FrqlHlUgz4",
+    title: "Dasvi ",
+    category: "Trailer",
+    description: "From counting votes to counting the days left for his 10th grade exams. Will Gangaram pass with flying colours?",
+    creator: "Netflix India",
+    uploadDate: "23-03-2022",
+    url: "https://youtu.be/-FrqlHlUgz4"
+  },
+  {
+    _id:"IU2ttJ73h2Y",
+    title: "Lift Karadey - Adnan Sami",
+    category: "Song",
+    description: "Presenting ‘Lift Karade’ music video sung & composed by Adnan Sami.",
+    creator: "SonyMusicIndiaVEVO",
+    uploadDate: "19-03-2022",
+    url: "https://youtu.be/IU2ttJ73h2Y"
+  },
+  {
+    _id:"hgi2MYAFgE8",
+    title: "Gulabi Aankhen | Sanam",
+    category: "Song",
+    description: "From one of the most peaceful and scenic islands of the beautiful country Maldives here's our #SANAMrendition of the super hit song 'Gulabi Aankhen' from the 1970 movie 'The Train'. ",
+    creator: "Sanam",
+    uploadDate: "25-11-2015",
+    url: "https://youtu.be/hgi2MYAFgE8"
+  },
+  {
+    _id:"GWZ1c9Ovswc",
+    title: "Gravitas: Shanghai: Residents beaten up by healthcare workers",
+    category: "News",
+    description: "China's latest Wuhan virus outbreak has led to chaos. In Shanghai, residents are being beaten up and forced into centralised quarantines. Palki Sharma Upadhyay brings you 5 videos that expose the truth about China's Zero Covid strategy. ",
+    creator: "WION",
+    uploadDate:  "05-04-2022",
+    url: "https://youtu.be/GWZ1c9Ovswc"
+  },
+  {
+    _id:"6QoDVoKumMs",
+    title: "Roomies",
+    category: "mini-web series",
+    description: "Check out this ultimate battle of roomies vs ration ft. Gagan Arora, Nikhil Vijay, Badri & Anushka.",
+    creator: "Alright!",
+    uploadDate:  "05-04-2022",
+    url: "https://youtu.be/6QoDVoKumMs"
+  },
+  {
+    _id:"8aBVZVGLO2Q",
+    title: "Life Of An Engineer",
+    category: "mini-web series",
+    description: "Living the life of an engineer and looking for strong problem-solving skills. Premium content. Job opportunities. All in one place. Sounds like a far-fetched dream?",
+    creator: "Alright!",
+    uploadDate: "03-04-2022",
+    url: "https://youtu.be/8aBVZVGLO2Q"
+  },
+  {
+    _id:"GCEXzUthGMo",
+    title: "Engineering Job Interview | Standup Comedy By Rohan Gujral",
+    category: "Standup Comedy",
+    description: "Job toh mili nahi par experience bada sahi sa raha. Job mil gayi dusri company mein badme laken sabse bakchod interviewers issi company mein they. Agar job mil gayi hoti yaha par toh shayad comedy na karra hota coz life vaise hi entertaining hoti. Share with all your friends and definitely with your boss. If you don't have a boss don't worry, tumhari bhi baari aayegi.",
+    creator: "Rohan Gujral",
+    uploadDate: "11-04-2020",
+    url: "https://youtu.be/GCEXzUthGMo"
+  },
+  {
+    _id:"qkxuFKqJXWY",
+    title: "Roommate - Stand Up Comedy Ft. Anubhav Singh Bassi",
+    category: "Standup Comedy",
+    description: "Stand Up Comedy",
+    creator: "Anubhav Singh Bassi",
+    uploadDate: "16-04-2021",
+    url: "https://youtu.be/qkxuFKqJXWY"
   },
 ];
