a5c5ff0e12dd9db3e784fdd3c3ca0d39e789d7df
Ravi kumar <1002kumarravi@gmail.com>
2022-04-25 12:59:21 +0530

    removed console.log

--- src/pages/watch-later/Watch-later.jsx
+++ src/pages/watch-later/Watch-later.jsx
@@ -16,7 +16,6 @@ export const WatchLater = () => {
                 watchLaterVideos: getWatchLaterData.watchlater
             }
         })
-        console.log(getWatchLaterData)
     }
 
     useEffect(() => {
